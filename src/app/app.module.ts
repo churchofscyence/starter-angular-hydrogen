3284a0af8bf78d7e7dd1fe5b24bc4b52dc7db3d8
Johnny Cowboy <johnnycowboy3033@gmail.com>
2023-10-22 01:30:56 -0400

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,28 @@ import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from "@angular/router";
+import { WelcomeComponent } from './welcome/welcome.component';
+
+let appRoutes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
