94c325b07e3f463a23f52ba5ab253a63eef3c5da
Johnny Cowboy <johnnycowboy3033@gmail.com>
2023-11-15 08:12:36 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {BannerComponent} from "./banner/banner.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [
+                AppComponent,
+                BannerComponent,
+                NavbarComponent,
+                FooterComponent
+    ]
   }));
 
   it('should create the app', () => {
